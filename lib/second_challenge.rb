834a91aba9067002351a5f9e346b7958fb9770aa
Kristen Papke <klpapke215@gmail.com>
2018-07-16 02:01:46 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,11 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}, 
-      :matriarch => {}, 
-      :hero => {}, 
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
       :hero_friends => []
-      }, 
+      },
     :capulet => {
       :patriarch => {},
       :matriarch => {},
@@ -13,5 +13,5 @@ def second_challenge
       :heroine_friends => []
       }}
 
-  
+
 end
